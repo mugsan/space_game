abaeef07c4eaf2b2aa959d123619d6f50bcc0402
mugsan <mugsan@gmail.com>
2014-11-28 09:08:26 +0100

    Well hello there

---
+++ src/client/GameViewController.java
@@ -0,0 +1,10 @@
+package client;
+
+/**
+ * Created by magnusbjork on 24/11/14.
+ */
+public class GameViewController {
+    public static void main(){
+        System.out.println("Wollo Herd");
+    }
+}
